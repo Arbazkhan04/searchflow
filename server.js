09849cc7fa63214189705ac5c9a4214b2cd6de43
Arbazkhan04 <arbaz582021@gmail.com>
2024-12-07 09:13:49 -0500

    setup done

---
+++ server.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const connectDb = require("./db/connect.js");
+const errorHandler = require("./middleware/errorHandler.js");
+const path = require('path'); 
+
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+// Expose the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+
+// Log every request to the console
+app.use(morgan(":method :url :status :response-time ms"));
+
+// Routes
+const userManagementRoutes = require("./routes/userManagementRouter.js");
+
+
+
+app.use("/api/userManagementRoutes", userManagementRoutes);
+
+
+
+// Error Handling Middleware
+app.use(errorHandler);
+
+
+// Start the server
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URL);
+    console.log("Database connected");
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
